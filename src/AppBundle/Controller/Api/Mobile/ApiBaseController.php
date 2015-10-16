7812c722bbd678134d306a6b0a8c73286c902aae
ubuntu12_lab <ubuntu12_924@ubuntu.com>
2015-10-16 20:15:27 +0800

    api mobile

---
+++ src/AppBundle/Controller/Api/Mobile/ApiBaseController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace AppBundle\Controller\Api\Mobile;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class ApiBaseController extends Controller
+{
+    public function userAction(Request $request)
+    {
+      // replace this example code with whatever you need
+
+      return new Response("{ data: { value: 'empty' } }");
+    }
+}
