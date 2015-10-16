7ce242131a85bbf19a2f08c4b83da2d7376d1eeb
ubuntu12_lab <ubuntu12_924@ubuntu.com>
2015-10-17 00:29:12 +0800

    remove entity from front control for temp

--- src/AppBundle/Controller/FrontController.php
+++ src/AppBundle/Controller/FrontController.php
@@ -2,7 +2,7 @@
 
 namespace AppBundle\Controller;
 
-use AppBundle\Entity\Post;
+// use AppBundle\Entity\Post;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -15,35 +15,35 @@ class FrontController extends Controller
       return $this->render('front/index.html.twig');
     }
 
-    public function postsAction(Request $request)
-    {
-      // replace this example code with whatever you need
-      $posts = $this->getDoctrine()
-        ->getRepository('AppBundle:Post')
-        ->findAll();
+    // public function postsAction(Request $request)
+    // {
+    //   // replace this example code with whatever you need
+    //   $posts = $this->getDoctrine()
+    //     ->getRepository('AppBundle:Post')
+    //     ->findAll();
 
-      $list = array(1, 2);
+    //   $list = array(1, 2);
 
-      return $this->render('front/index.html.twig', array( 'posts' => $posts ));
-    }
+    //   return $this->render('front/index.html.twig', array( 'posts' => $posts ));
+    // }
 
-    public function showAction(Request $request, $id)
-    {
-      $post = $this->getDoctrine()
-        ->getRepository('AppBundle:Post')
-        ->find($id);
+    // public function showAction(Request $request, $id)
+    // {
+    //   $post = $this->getDoctrine()
+    //     ->getRepository('AppBundle:Post')
+    //     ->find($id);
 
-      return $this->render('front/posts/show.html.twig', array( 'post' => $post ));
-    }
+    //   return $this->render('front/posts/show.html.twig', array( 'post' => $post ));
+    // }
 
-    public function createPost()
-    {
-        $post = new Post();
-        $post->setTitle('A Post');
-        $post->setContent('Content');
+    // public function createPost()
+    // {
+    //     $post = new Post();
+    //     $post->setTitle('A Post');
+    //     $post->setContent('Content');
 
-        $em = $this->getDoctrine()->getManager();
-        $em->persist($post);
-        $em->flush();      
-    }
+    //     $em = $this->getDoctrine()->getManager();
+    //     $em->persist($post);
+    //     $em->flush();      
+    // }
 }
